9cbc444bb4ddf6dfbce1c2636adc62ee6adbcae5
hzuellig <zuellig@internauta.ch>
2023-03-08 08:04:39 +0100

    cleaning

---
+++ public/sketch.js
@@ -0,0 +1,107 @@
+var socket;
+
+var moves = 4; //anzahl unterschiedliche bewegungen 
+
+var settings = {
+    start: false,
+    finished: false,
+    queue:0,
+    ballx:-100,
+    bally:window.innerHeight/2,
+    radius:50,
+    stepx:10,
+    stepy:5
+}
+
+function setup(){
+    createCanvas(window.innerWidth, window.innerHeight)
+    socket=io.connect("http://localhost:3000/");
+    socket.emit("settings", settings);
+    socket.on("canIstart", startDrawing);
+    socket.on("settings", setSettings);
+   
+    background(0);
+
+    settings.bally= random(window.innerHeight);
+}
+
+function draw(){
+
+    if(settings.start==false){
+        socket.emit("canIstart", settings);
+    }
+    if(settings.start==true && settings.finished==false){
+        //draw
+        makeMove();
+
+        if (settings.ballx > width-settings.radius/2) {
+            
+            settings.ballx = settings.ballx - width; //zurücksetzen um die eigene Breite
+
+            socket.emit("next", settings);//start next ball
+            settings.finished=true;
+        }
+        
+        
+    }
+
+}
+
+function startDrawing(data){
+    settings.start = data.start;
+    settings.ballx=data.ballx;
+    settings.bally=data.bally;
+
+    //console.log("I got "+settings.ballx+" and "+ settings.start)
+}
+
+function setSettings(data) {
+    settings.socketid = data.socketid;
+    settings.queue = data.queue;
+    //console.log(settings.queue);
+    //setting=data;
+}
+
+
+function makeMove(){
+    let move = settings.queue % moves;
+    //console.log("move "+move)
+    switch(move){
+        case(0):
+        move_1();
+        break;
+        case(1):
+        move_1()
+        break;
+        case(2):
+        move_1()
+        break;
+        case(3):
+        move_1()
+        break;
+    }
+}
+
+function move_1(){
+        background(0, 20);
+        fill(255,255,0);
+        noStroke();
+        arc(width/2, height/2, width, height, 0, PI);
+
+        fill(255,0,0);
+    
+        ellipse(settings.ballx, settings.bally, settings.radius);
+        settings.ballx+=settings.stepx;
+}
+function move_2(){
+
+    
+}
+function move_3(){
+   
+
+}
+function move_4(){
+    
+   
+}
---
+++ server.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var app = express();
+var server = app.listen(3000);
+app.use(express.static("public"));
+var socket = require("socket.io");
+var io = socket(server);
+io.sockets.on("connection", newConnection);
+
+
+var clientId = 0; //Nummerierung der Clients
+var activeScreen = 0;//Keep Track which client is active
+
+var ball={
+    ballx:0,
+    bally:0
+};
+
+function newConnection(socket) {
+   
+    socket.on("settings", Msg);
+
+    function Msg(data) {
+
+        let settings = {
+            queue: clientId,
+            socketid: socket.id
+        }
+        clientId++;
+       
+        io.to(socket.id).emit("settings", settings);
+    }
+
+    socket.on("canIstart", Msgstart);
+
+    function Msgstart(data) {
+        
+        if (data.queue == activeScreen) {
+            data.start = true;
+            if(activeScreen > 0){
+                data.ballx = ball.ballx;
+                data.bally= ball.bally;
+            }
+            
+
+            io.to(socket.id).emit("canIstart", data);
+        }
+    }
+
+    socket.on("next", setNext);
+
+    function setNext(data) {
+       ball=data;
+       activeScreen = activeScreen + 1;
+    }
+
+
+}
